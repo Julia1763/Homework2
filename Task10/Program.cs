85954a2dc0eb633af45a16c893371e48c686bc16
Julia <odintsova.julia@mail.ru>
2022-08-11 23:12:43 +0300

    у

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int num = int.Parse(Console.ReadLine());
-int num2 = num/10;
-Console.WriteLine($"Вторая цифра числа {num} является {num2%10}");
+int num2 = (num/10)%10;
+Console.WriteLine($"Вторая цифра числа {num} является {num2}");
