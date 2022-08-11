a61e5241444860ce19aa854a254b89636e40a643
Julia <odintsova.julia@mail.ru>
2022-08-11 23:33:01 +0300

    Написали решение к 15 задаче

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру: ");
+int days = int.Parse(Console.ReadLine());
+if (days>5)
+{
+  Console.WriteLine("Выходной");  
+  if (days>7)
+  {
+    Console.WriteLine("День недели под таким номером отсутствует"); 
+  }
+}
+else
+{
+   Console.WriteLine("Не является выходным");
+}
